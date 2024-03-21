1c4233b933a560bf1962f2ae96167f051d5d12f7
Jaco <jacovancr@gmail.com>
2024-03-21 14:58:14 +0100

    More logs

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 	"uptime-monitor/internal/mail"
 	"uptime-monitor/internal/monitor"
 	"uptime-monitor/internal/storage"
@@ -41,12 +44,43 @@ func main() {
 
 	go monitor.Start()
 
-	// Block waiting for shutdown signal
-	sigs := make(chan os.Signal, 1)
-	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM) // Listen for SIGINT and SIGTERM 
-	<-sigs // Block until channel receives a signal
+	// Start a webserver that simply returns 200 OK for all requests
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+	log.Println("Defined the routes")
+
+	server := &http.Server{Addr: ":8080"}
+	log.Println("Server created");
+
+	go func() {
+		fmt.Println("Server is starting...")
+		if err := server.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("Server failed to start: %v\n", err)
+		}
+	}()
 	
-	fmt.Println("Shutting down")
+	log.Println("Setting up signal handlers...")
+	// Blocking main and waiting for shutdown signal
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("Waiting for shutdown signal...")
+	<-sigs
+
+	fmt.Println("Shutting down server...")
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server Shutdown Failed:%+v", err)
+	}
+	fmt.Println("Server gracefully stopped")
+
+	// Stop your monitor or any other services below
 	monitor.Stop()
-	fmt.Println("Shut down");
+	fmt.Println("Monitor stopped. Exiting now.")
 }
